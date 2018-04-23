1ffbe4dcaaf7007deccca2f305c1730e5f663208
Sean McCarthy <smmccrthy@gmail.com>
2018-04-23 11:00:00 -0400

    inital commit

---
+++ Food Tracker/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by Sean McCarthy on 4/22/18.
+//  Copyright © 2018 Sean McCarthy. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+}
